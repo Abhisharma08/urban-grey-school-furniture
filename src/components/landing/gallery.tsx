3760f263d889a0ad02bfc391f03910c70f5c329e
charu <charup640@gmail.com>
2025-08-30 15:54:28 +0530

    Updated content of homepage

--- src/components/landing/gallery.tsx
+++ src/components/landing/gallery.tsx
@@ -34,7 +34,7 @@ export function Gallery() {
             Products <span className="text-accent">Gallery</span>
           </h2>
           <p className="mx-auto mt-6 max-w-3xl text-lg text-foreground/80 md:text-xl">
-        Timeless, functional, and built for collaboration — explore Urban Grey’s signature conference room collections.
+        Designed for comfort. Crafted for community. Explore our best-selling cafeteria collections
           </p>
         </div>
         <div className="mt-16">
